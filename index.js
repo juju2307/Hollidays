cdbe4f301cf862857693a7106e24a87f475a94a5
juju2307 <metzjulie2@gmail.com>
2021-09-21 16:52:57 +0200

    fin du projet

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
 
-const {getCode, getName} = require("country-list");
+const {getCode} = require("country-list");
 const axios = require("axios");
 
-console.log(getName('IS'));
-console.log(getCode('Iceland'));
-console.log(getCode('Nowhere-to-be-found-land'));
+const figlet = require('figlet');
+
+figlet('Good Holidays!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
 
 
+const args = process.argv.slice(2);
+const country = getCode(args[0]);
+const year = args[1] ? args[1] : new Date().getFullYear();
 
+
+axios
+.get(`https://date.nager.at/Api/v1/Get/${country}/${year}`)
+.then(response => handleResponse(response.data))
+.catch(err => console.log(err));
+
+handleResponse = holidays => {
+    holidays.map(holiday => {
+      console.log(`Holiday: ${holiday.name}\nDate: ${holiday.date}\n`);
+});
+}; 
